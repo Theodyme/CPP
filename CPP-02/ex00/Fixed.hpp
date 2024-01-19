87b1decaee6299ec8acc53a9328dea65e4da2175
Theophane Place <flplace@bess-f1r1s7.clusters.42paris.fr>
2024-01-19 19:03:48 +0100

    starting cpp02

---
+++ CPP-02/ex00/Fixed.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+private:
+	int			_integer;
+	static int	_fraction;
+public:
+	Fixed(/* args */) {}
+	~Fixed() {}
+};
+#endif
