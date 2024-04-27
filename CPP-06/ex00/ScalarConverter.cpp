ce336ea1f081095d7168c41e1be284b5a3ea7780
Theophane Place <flplace@bess-f2r3s10.clusters.42paris.fr>
2024-04-27 18:12:23 +0200

    ex01 c06

--- CPP-06/ex00/ScalarConverter.cpp
+++ CPP-06/ex00/ScalarConverter.cpp
@@ -1,10 +1,7 @@
 #include "ScalarConverter.hpp"
 
 /*                                CONSTRUCTEURS                               */
-ScalarConverter::ScalarConverter()
-{
-
-}
+ScalarConverter::ScalarConverter() { }
 
 ScalarConverter::ScalarConverter(const ScalarConverter&src)
 {
@@ -12,42 +9,187 @@ ScalarConverter::ScalarConverter(const ScalarConverter&src)
 }
 /*                                DESTRUCTEURS                                */
 
-ScalarConverter::~ScalarConverter()
-{
+ScalarConverter::~ScalarConverter() { }
 
-}
 /*                              OPERATOR OVERLOAD                             */
 
 /*                                   METHODS                                  */
-void ScalarConverter::convert(const std::string arg)
+
+int	ScalarConverter::trimming(void)
 {
-	std::string trimmed = arg;
-
-
-	for (size_t i = 0; i < trimmed.size(); i++)
+	for (size_t i = 0; i < this->_trarg.size(); i++)
 	{
-		if (std::isspace(trimmed[i])) 
+		if (std::isspace(this->_trarg[i]) == true)
 		{
-			trimmed.erase(i, 1);
+			this->_trarg.erase(i, 1);
 			--i;
 		}
 	}
-	if (!trimmed[0])
+	if (!_trarg[0])
 	{
 		std::cout << "Nothing to convert!\n";
+		return 1;
+	}
+	return 0;
+}
+
+void ScalarConverter::firstCheck(void)
+{
+	char *suffix;
+
+	if (this->_trarg.size() == 1)
+	{
+		if (isdigit(this->_trarg[0]) == false)
+		{
+			this->_char = this->_trarg[0];
+			this->_type = CHAR;
+			return ;
+		}
+	}
+
+	try
+	{
+		this->_int = strtol(this->_trarg.c_str(), &suffix, 10);
+		if (errno == ERANGE || this->_int > INT_MAX || this->_int < INT_MIN)
+			throw ScalarConverter::Overflow();
+		if (suffix[0])
+		{
+			this->_double = strtod(this->_trarg.c_str(), &suffix);
+			if (errno == ERANGE)
+				throw ScalarConverter::Overflow();
+			if (suffix[0])
+			{
+				this->_float = strtof(this->_trarg.c_str(), &suffix);
+				if (errno == ERANGE)
+					throw ScalarConverter::Overflow();
+				if (strcmp(suffix, "f"))
+					this->_type = INVALID;
+				else
+					this->_type = FLOAT;
+			}
+			else
+				this->_type = DOUBLE;
+			return ;
+		}
+		this->_type = INT;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	return ;
+}
+
+void ScalarConverter::printChar()
+{
+		std::cout << PU << "Char: " << WH;
+		std::cout << this->_char << std::endl;
+		std::cout << PU << "Int: " << WH;
+		std::cout << static_cast<int>(this->_char) << std::endl;
+		std::cout << PU << "Float: " << WH;
+		std::cout << static_cast<float>(this->_char) << "f" << std::endl;
+		std::cout << PU << "Double: " << WH;
+		std::cout << static_cast<double>(this->_char) << std::endl;
+}
+
+void ScalarConverter::printInt()
+{
+	std::cout << PU << "Char: " << WH;
+	if (this->_int >= 0 && this->_int <= 255 && isprint(this->_int))
+		std::cout << static_cast<char>(this->_int) << std::endl;
+	else
+		std::cout << "non displayable.\n";
+	std::cout << PU << "Int: " << WH;
+	std::cout << this->_int << std::endl;
+	std::cout << PU << "Float: " << WH;
+	std::cout << static_cast<float>(this->_int) << "f" << std::endl;
+	std::cout << PU << "Double: " << WH;
+	std::cout << static_cast<double>(this->_int) << std::endl;
+}
+
+void ScalarConverter::printFloat()
+{
+	std::cout << PU << "Char: " << WH;
+	if (this->_float >= 0 && this->_float <= 255 && isprint(this->_float))
+		std::cout << static_cast<char>(this->_float) << std::endl;
+	else
+		std::cout << "non displayable.\n";
+	std::cout << PU << "Int: " << WH;
+	if (this->_float >= static_cast<float>(INT_MIN) && this->_float <= static_cast<float>(INT_MAX))
+		std::cout << static_cast<int>(this->_float) << std::endl;
+	else
+		std::cout << "impossible.\n";
+	std::cout << PU << "Float: " << WH;
+	std::cout << this->_float << "f" << std::endl;
+	std::cout << PU << "Double: " << WH;
+	std::cout << static_cast<double>(this->_float) << std::endl;
+}
+
+void ScalarConverter::printDouble()
+{
+	std::cout << PU << "Char: " << WH;
+	if (this->_double >= 0 && this->_double <= 255 && isprint(this->_double))
+		std::cout << static_cast<char>(this->_double) << std::endl;
+	else
+		std::cout << "non displayable.\n";
+	std::cout << PU << "Int: " << WH;
+	if (this->_double >= INT_MIN && this->_double <= INT_MAX)
+		std::cout << static_cast<int>(this->_double) << std::endl;
+	else
+		std::cout << "impossible.\n";
+	std::cout << PU << "Float: " << WH;
+	if (this->_double >= FLT_MIN && this->_double <= FLT_MAX)
+		std::cout << static_cast<float>(this->_double) << "f" << std::endl;
+	else
+		std::cout << "impossible.\n";
+	std::cout << PU << "Double: " << WH;
+	std::cout << this->_double << std::endl;
+}
+
+void ScalarConverter::printer(Type type)
+{
+	switch (type)
+	{
+	case CHAR:
+		this->printChar();
+		break;
+
+	case INT:
+		this->printInt();
+		break;
+
+	case FLOAT:
+		this->printFloat();
+		break;
+
+	case DOUBLE:
+		this->printDouble();
+		break;
+
+	default:
+		break;
+	}
+}
+
+void ScalarConverter::convert(const std::string arg)
+{
+	ScalarConverter converter;
+
+	converter._trarg = arg;
+	if (converter.trimming() == 1)
+		return;
+	converter.firstCheck();
+	if (converter._type == INVALID)
+	{
+		std::cout << RE << "Error:" << WH << " invalid input\n";
 		return ;
 	}
-	// std::cout << "trimmed: " << trimmed << "\n";
-	char *last;
-
-	int intType = strtol(trimmed.c_str(), &last, 10);
-	if (last)
-	{
-		
-	}
-
-
-
-
+	std::cout << std::showpoint << std::setprecision(5);
+	converter.printer(converter._type);
 	return;
+}
+
+const char* ScalarConverter::Overflow::what() const throw()
+{
+	return RE "Error:" WH " overflow";
 }
--- CPP-06/ex00/ScalarConverter.hpp
+++ CPP-06/ex00/ScalarConverter.hpp
@@ -2,21 +2,61 @@
 # define SCALARCONVERTER_HPP
 
 # include <iostream>
+# include <iomanip>
 # include <string>
+# include <cerrno>
+# include <cstring>
+# include <cstdlib>
 # include <cctype>
+# include <climits>
+# include <cfloat>
+
+# define RE "\033[31m"
+# define WH "\033[0m"
+# define PU "\033[35m"
+
+enum Type {
+	INT, // 0
+	DOUBLE, // 1
+	FLOAT, // 2
+	CHAR, // 3
+	UNDISPLAYABLE, // 4
+	INVALID // 5
+};
 
 class ScalarConverter
 {
 	private:
+		std::string _trarg;
+		Type _type;
+
+		long int	_int;
+		double _double;
+		float _float;
+		char _char;
 	// CONSTRUCTEURS
 		ScalarConverter();
 		ScalarConverter(const ScalarConverter&src);
-	public:
 	// DESTRUCTEURS
 		~ScalarConverter();
+	public:
 	// OPERATOR OVERLOAD
 	// METHODS
-		static void convert(const std::string arg); 
+		int	trimming(void);
+		void firstCheck(void);
+		void printChar();
+		void printInt();
+		void printFloat();
+		void printDouble();
+		void printer(Type type);
+		static void convert(const std::string arg);
+
+	class Overflow : public std::exception
+	{
+		public:
+			virtual const char* what() const throw();
+
+	};
 };
 
 # endif
