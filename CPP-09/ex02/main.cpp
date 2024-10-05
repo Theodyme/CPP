457da3c024f04a419a8ee30dd0fdbd089dc36c85
Theophane Place <flplace@made-f0Cr2s2.clusters.42paris.fr>
2024-10-05 18:03:04 +0200

    bleh

--- CPP-09/ex02/main.cpp
+++ CPP-09/ex02/main.cpp
@@ -2,37 +2,57 @@
 
 
 template <typename C>
-void fill(char **av, C &container)
+void print(C &container, std::string type)
+{
+	if (type == "VECTOR")
+		std::cout << "vector: [";
+	else
+		std::cout << "deque: [";
+	for (int i = 0; container[i]; i++)
+		std::cout << container[i] << ", ";
+	std::cout << "]" << std::endl;
+}
+
+template <typename C>
+int fill(char **av, C &container)
 {
 	char *suffix;
 	for (int i = 1; av[i]; i++)
 	{
-		container.push_back(strtol(av[i], &suffix, 10));
-		std::cout << container[i - 1] << ", ";
-		if (*suffix != '\0')
+		long value = strtol(av[i], &suffix, 10);
+		if (*suffix != '\0' || value < 0)
 		{
 			std::cerr << "Error: invalid argument\n";
-			break ;
+			return 1;
 		}
-		std::cout << std::endl;
+		container.push_back(value);
 	}
+	return 0;
 }
 
 template <typename T>
-void divideVector(std::vector<T> &vector)
+std::vector<T> sortVector(std::vector<T> &vector)
 {
-	std::vector<std::pair<T, T>>	divided;
+	if (vector.size <= 1)
+		return vector;
 
-	for (int i = 0; vector[i]; i = i + 2)
+	std::vector<std::pair<T, T> >	divided;
+
+	for (int i = 0; vector[i]; i += 2)
 	{
-		if ((i + 1) <= vector.size())
-		{
-			divided.insert(std::pair<T, T>(vector[i], vector[i + 1]);)
-		}
+		if ((i + 1) < vector.size())
+			divided.insert(std::pair<T, T>(vector[i], vector[i + 1]));
 		else
-		
-
+			divided.insert(std::pair<T, T>(vector[i], T()));
 	}
+
+	// tri pour recuperer chaque element + plus par paire
+
+	// recursion dans le sort des elements les + grands
+	// sortVector();
+
+	//
+
 }
 
 
@@ -45,12 +65,15 @@ int	main(int ac, char **av)
 	// remplit une stack de vecteurs et verifie si les arguments sont tous valides
 	std::vector<int> vector;
 	std::deque<int> deque;
-	fill(av, vector);
-	fill(av, deque);
+	if (fill(av, vector) == 1)
+		return 1;
+	if (fill(av, deque) == 1)
+		return 1;
 
+	print(vector, "VECTOR");
+	print(deque, "DEQUE");
 
-
-
+	divideVector(vector);
 	// recursion
 
 	// retour
