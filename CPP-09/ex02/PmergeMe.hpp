1a2ca57e3afaa85874841749f0b92d9a14214572
Theo <theo@localhost.localdomain>
2024-11-22 11:00:56 +0100

    final push

--- CPP-09/ex02/PmergeMe.hpp
+++ CPP-09/ex02/PmergeMe.hpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <deque>
 #include <utility>
+#include <ctime>
 
 // Suite de Jacobsthal en dur
 const int jacobsthalIndices[] = {
@@ -12,3 +13,195 @@ const int jacobsthalIndices[] = {
 	43691, 87381, 174763, 349525, 699051, 1398101, 2796203, 5592405, 11184811,
 	22369621, 44739243, 89478485, 178956971, 357913941, 715827883, 1431655765
 };
+
+// Taille de la suite de Jacobsthal
+const size_t jacobsthalSize = sizeof(jacobsthalIndices) / sizeof(jacobsthalIndices[0]);
+
+template <typename C>
+void	printList(C &container)
+{
+	std::cout << "[";
+	for (size_t i = 0; i < container.size(); i++)
+	{	
+		std::cout << container[i];
+		if ((i + 1) != container.size())
+			std::cout << ", ";
+	}
+	std::cout << "]" << std::endl;
+}
+
+template <typename C>
+void	print(C &container, std::string type)
+{
+	if (type == "VECTOR")
+		std::cout << "vector: ";
+	else
+		std::cout << "deque: ";
+	printList(container);
+}
+
+template <typename C>
+int		fill(char **av, C &container)
+{
+	char *suffix;
+	for (int i = 1; av[i]; i++)
+	{
+		long value = strtol(av[i], &suffix, 10);
+		if (*suffix != '\0' || value <= 0)
+		{
+			std::cerr << "Error: invalid argument\n";
+			return 1;
+		}
+		container.push_back(value);
+	}
+	return 0;
+}
+
+template <typename T>
+std::pair<T, T>	sortPair(T &a, T &b)
+{
+	return	(a > b) ? std::make_pair(a, b) : std::make_pair(b, a);
+}
+
+template <typename T, typename C>
+void	binarySearch(C& list, T element) {
+	typename C::iterator it = std::lower_bound(list.begin(), list.end(), element);
+	list.insert(it, element);
+}
+
+
+//////////////////////// SORT VECTOR //////////////////////////
+
+template <typename T>
+std::vector<T> sortVector(std::vector<T> &initialList)
+{
+	if (initialList.size() <= 1)
+		return initialList;
+
+	std::vector<std::pair<T, T> >	paired;
+
+	for (size_t i = 0; i < initialList.size(); i += 2)
+	{
+		if ((i + 1) < initialList.size())
+			paired.push_back(sortPair(initialList[i], initialList[i + 1]));
+		else
+			paired.push_back(std::pair<T, T>(initialList[i], T()));
+	}
+	
+	// creation d'une liste temporaire contenant les plus grands elem. de chaque paire
+	std::vector<T> largerList;
+	for (size_t i = 0; i < paired.size(); i++)
+	{
+		largerList.push_back(paired[i].first);
+	}
+
+	// recursion dans le sort des elements les + grands
+	std::vector<T> sortedList = sortVector(largerList);
+	std::vector<bool>	isInserted(paired.size(), false);
+
+	// insertion des éléments appairés selon les indices de jacobsthal
+	for (size_t i = 0; i < jacobsthalSize; i++)
+	{
+		size_t idx = static_cast<size_t>(jacobsthalIndices[i]);
+		if (idx >= paired.size())
+		{
+			for (size_t newIdx = jacobsthalIndices[i - 1]; newIdx < paired.size(); newIdx++)
+			{
+				if (paired[newIdx].second != T() && !isInserted[newIdx])
+				{
+					binarySearch(sortedList, paired[newIdx].second);
+					isInserted[newIdx] = true;
+				}
+			}
+			break ;
+		}
+		if (paired[idx].second != T() && !isInserted[idx])
+		{
+			binarySearch(sortedList, paired[idx].second);
+			isInserted[idx] = true;
+		}
+
+		// fonction pour backtrack
+		if (i > 0)
+		{
+			for (size_t prevIdx = jacobsthalIndices[i - 1]; prevIdx < idx; prevIdx++)
+			{
+				if (paired[prevIdx].second != T() && !isInserted[prevIdx])
+				{
+					binarySearch(sortedList, paired[prevIdx].second);
+					isInserted[prevIdx] = true;
+				}
+			}
+		}
+	}
+	return sortedList;
+}
+
+
+
+//////////////////////// SORT DEQUE //////////////////////////
+
+template <typename T>
+std::deque<T> sortDeque(std::deque<T> &initialList)
+{
+	if (initialList.size() <= 1)
+		return initialList;
+
+	std::deque<std::pair<T, T> >	paired;
+
+	for (size_t i = 0; i < initialList.size(); i += 2)
+	{
+		if ((i + 1) < initialList.size())
+			paired.push_back(sortPair(initialList[i], initialList[i + 1]));
+		else
+			paired.push_back(std::pair<T, T>(initialList[i], T()));
+	}
+	
+	// creation d'une liste temporaire contenant les plus grands elem. de chaque paire
+	std::deque<T> largerList;
+	for (size_t i = 0; i < paired.size(); i++)
+	{
+		largerList.push_back(paired[i].first);
+	}
+
+	// recursion dans le sort des elements les + grands
+	std::deque<T> sortedList = sortDeque(largerList);
+	std::deque<bool>	isInserted(paired.size(), false);
+
+	// insertion des éléments appairés selon les indices de jacobsthal
+	for (size_t i = 0; i < jacobsthalSize; i++)
+	{
+		size_t idx = static_cast<size_t>(jacobsthalIndices[i]);
+		if (idx >= paired.size())
+		{
+			for (size_t newIdx = jacobsthalIndices[i - 1]; newIdx < paired.size(); newIdx++)
+			{
+				if (paired[newIdx].second != T() && !isInserted[newIdx])
+				{
+					binarySearch(sortedList, paired[newIdx].second);
+					isInserted[newIdx] = true;
+				}
+			}
+			break ;
+		}
+		if (paired[idx].second != T() && !isInserted[idx])
+		{
+			binarySearch(sortedList, paired[idx].second);
+			isInserted[idx] = true;
+		}
+
+		// fonction pour backtrack
+		if (i > 0)
+		{
+			for (size_t prevIdx = jacobsthalIndices[i - 1]; prevIdx < idx; prevIdx++)
+			{
+				if (paired[prevIdx].second != T() && !isInserted[prevIdx])
+				{
+					binarySearch(sortedList, paired[prevIdx].second);
+					isInserted[prevIdx] = true;
+				}
+			}
+		}
+	}
+	return sortedList;
+}
--- CPP-09/ex02/main.cpp
+++ CPP-09/ex02/main.cpp
@@ -1,62 +1,6 @@
 #include "PmergeMe.hpp"
 
-
-template <typename C>
-void print(C &container, std::string type)
-{
-	if (type == "VECTOR")
-		std::cout << "vector: [";
-	else
-		std::cout << "deque: [";
-	for (int i = 0; container[i]; i++)
-		std::cout << container[i] << ", ";
-	std::cout << "]" << std::endl;
-}
-
-template <typename C>
-int fill(char **av, C &container)
-{
-	char *suffix;
-	for (int i = 1; av[i]; i++)
-	{
-		long value = strtol(av[i], &suffix, 10);
-		if (*suffix != '\0' || value < 0)
-		{
-			std::cerr << "Error: invalid argument\n";
-			return 1;
-		}
-		container.push_back(value);
-	}
-	return 0;
-}
-
-template <typename T>
-std::vector<T> sortVector(std::vector<T> &vector)
-{
-	if (vector.size <= 1)
-		return vector;
-
-	std::vector<std::pair<T, T> >	divided;
-
-	for (int i = 0; vector[i]; i += 2)
-	{
-		if ((i + 1) < vector.size())
-			divided.insert(std::pair<T, T>(vector[i], vector[i + 1]));
-		else
-			divided.insert(std::pair<T, T>(vector[i], T()));
-	}
-
-	// tri pour recuperer chaque element + plus par paire
-
-	// recursion dans le sort des elements les + grands
-	// sortVector();
-
-	//
-
-}
-
-
-int	main(int ac, char **av)
+int		main(int ac, char **av)
 {
 	// pas d'argument a parser
 	if (ac == 1)
@@ -70,12 +14,44 @@ int	main(int ac, char **av)
 	if (fill(av, deque) == 1)
 		return 1;
 
-	print(vector, "VECTOR");
-	print(deque, "DEQUE");
 
-	divideVector(vector);
-	// recursion
+	std::cout << "//////// VECTOR //////////" << std::endl;
 
-	// retour
+	std::cout << std::endl;
+	std::cout << "Before: ";
+	printList(vector);
+
+	std::clock_t startV = std::clock();
+	std::vector<int> sortedVector = sortVector(vector);
+	std::clock_t endV = std::clock();
+
+	std::cout << "After: ";
+	printList(sortedVector);
+	std::cout << std::endl;
+
+	double timeV = 1000000.0 * (endV - startV) / CLOCKS_PER_SEC;
+
+	std::cout << "Time to process a range of " << vector.size() << " elements with std::vector: " << timeV << " us" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "//////// DEQUE //////////" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Before: ";
+	printList(deque);
+
+	std::clock_t startD = std::clock();
+	std::deque<int> sortedDeque = sortDeque(deque);
+	std::clock_t endD = std::clock();	
+
+	std::cout << "After: ";
+	printList(sortedDeque);
+	std::cout << std::endl;
+
+	double timeD = 1000000.0 * (endD - startD) / CLOCKS_PER_SEC;
+
+	std::cout << "Time to process a range of " << deque.size() << " elements with std::deque: " << timeD << " us" << std::endl;
+
+
 	return 0;
 }
